ec7b0777fee7c0581aefdd5a34d153a1b106f3b6
EllaKaye <ella.kaye@gmail.com>
2025-12-27 11:10:10 +0000

    setter for notation

--- R/siteswap.R
+++ R/siteswap.R
@@ -2,7 +2,13 @@
 siteswap <- S7::new_class(
   "siteswap",
   properties = list(
-    notation = S7::class_character
+    notation = S7::new_property(
+      class = S7::class_character,
+      setter = function(self, value) {
+        self@notation <- as.character(value)
+        self
+      }
+    )
   ),
   package = "jugglr"
 )
