c56a706168111e8a1675a16550256c71c5e5891f
EllaKaye <ella.kaye@gmail.com>
2025-12-28 20:18:31 +0000

    Validators for notation

--- R/siteswap.R
+++ R/siteswap.R
@@ -2,11 +2,22 @@
 siteswap <- S7::new_class(
   "siteswap",
   properties = list(
-    notation = S7::class_character
+    notation = S7::new_property(
+      class = S7::class_character,
+      validator = function(value) {
+        if (length(value) != 1) {
+          "must be length 1"
+        }
+      },
+    )
   ),
   package = "jugglr"
 )
 
+# TODO: add validators
+# Check regex for siteswap
+# Can I validate `notation` for vanilla here, given `notation` is defined in parent class?
+# TODO: tolower notaion?
 #' @export
 vanilla_siteswap <- S7::new_class(
   "vanilla_siteswap",
@@ -48,10 +59,16 @@ vanilla_siteswap <- S7::new_class(
       }
     )
   ),
+  validator = function(self) {
+    if (!(grepl("^[a-zA-Z0-9]+$", self@notation))) {
+      "@notation must only contain digits and letters"
+    }
+  },
   parent = siteswap,
   package = "jugglr"
 )
 
-method(print, vanilla_siteswap) <- function(x, ...) {
+# TODO: useful print method!
+S7::method(print, vanilla_siteswap) <- function(x, ...) {
   cat("This is vanilla siteswap")
 }
